6e93fb2f6bf3feb15eafe7b7e5ad743d44a08757
LaFleurFromMoscow <ivanovskiyroma@mail.ruivanovskiyroma@mail.ruivanovskiyroma@mail.ru>
2023-07-30 18:14:35 +0300

    сделал тесты для презентера, сдача на ревью

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -64,39 +64,37 @@ final class MovieQuizUITests: XCTestCase {
     }
     
     
-    func testGameFinish() {
+    func testGameFinishAlert() {
         sleep(2)
         for _ in 1...10 {
             app.buttons["No"].tap()
-
-            sleep(2)
+            sleep(3)
         }
         
-        let alert = app.alerts["Game results"]
-        
-        XCTAssertTrue(alert.exists)
-        XCTAssertTrue(alert.label == "Этот раунд окончен!")
-        XCTAssertTrue(alert.buttons.firstMatch.label == "Сыграть ещё раз")
+        let gameFinishAlert = app.alerts["Этот раунд окончен!"]
+        XCTAssertTrue(gameFinishAlert.exists)
+        XCTAssertTrue(gameFinishAlert.label == "Этот раунд окончен!")
+        XCTAssertTrue(gameFinishAlert.buttons.firstMatch.label == "Сыграть ещё раз")
     }
     
-  /*  func testAlertDismiss() {
-        sleep(2)
-        for _ in 1...10 {
-            app.buttons["No"].tap()
-            sleep(2)
-        }
-        
-        let alert = app.alerts["Game results"]
-        alert.buttons.firstMatch.tap()
-        
-        sleep(2)
-        
-        let indexLabel = app.staticTexts["Index"]
-        
-        XCTAssertFalse(alert.exists)
-        XCTAssertTrue(indexLabel.label == "1/10")
-    }
-*/
+    /*  func testAlertDismiss() {
+     sleep(2)
+     for _ in 1...10 {
+     app.buttons["No"].tap()
+     sleep(2)
+     }
+     
+     let alert = app.alerts["Game results"]
+     alert.buttons.firstMatch.tap()
+     
+     sleep(2)
+     
+     let indexLabel = app.staticTexts["Index"]
+     
+     XCTAssertFalse(alert.exists)
+     XCTAssertTrue(indexLabel.label == "1/10")
+     }
+     */
 }
 
 
